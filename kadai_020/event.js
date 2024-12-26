7cb12eada6963b8cfdcc3e1027506152089f237a
iwataka-osaka <takafumi.iwata.dev@gmail.com>
2024-12-26 14:41:41 +0900

    再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,10 @@
  // btnというidを持つHTML要素を取得し、定数に代入する
  const Btn = document.getElementById('btn');
+ // btnというidを持つHTML要素を取得し、定数に代入する
+ const text = document.getElementById('text');
  
  // HTML要素がクリックされたときにイベント処理を実行する
  Btn.addEventListener('click', () => {
-   // ボタンをクリックしました。
-   document.getElementById('text').textContent ="ボタンをクリックしました";
+  // ボタンをクリックしました。
+  text.textContent="ボタンをクリックしました";
  });
